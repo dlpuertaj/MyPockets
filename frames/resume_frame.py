304a9aff670713cb2026e8dce073a5994d7c24ae
David Leonardo <dlpuerta@gmail.com>
2022-03-10 02:37:33 -0500

    Changing Resume frame

--- frames/resume_frame.py
+++ frames/resume_frame.py
@@ -8,6 +8,8 @@ from services import Services as serve
 class ResumeFrame(Frame):
     """ Iit method that instantiates service and resume table"""
 
+    months = ("01","02","03","04","05","06",
+              "07","08","09","10","11","12")
     def __init__(self, root):
         Frame.__init__(self, root)
         self.resume_table = ttk.Treeview(self)
@@ -55,8 +57,36 @@ class ResumeFrame(Frame):
         for column in columns:
             self.resume_table.heading(column, text=column, anchor=W)
 
-    """ Method that adds the database data to the resume table"""
+    def build_resume_tables(self):
+        expense_types = serve.get_expense_type_names()
+        for month in self.months:
+            moth_resume_data = self.serve.get_resume_data((month,))
+            if len(moth_resume_data) == 0:
+                resume_data = ('None', 0)
+            else:
+                for data in resume_data:
+                    for column_index in range(len(expense_types)):
+                        column = self.resume_table.column(column_index, option='id')
+                        if data[0] == column:
+                            if payroll == 0:
+                                percent = 0
+                            else:
+                                percent = data[1] / payroll[0]
+                            amount_for_table.append(data[1])
+                            percent_for_table.append(str(percent * 100) + "%")
+                            sum_percent += percent
+                        else:
+                            amount_for_table.append(0)
+                            percent_for_table.append("0%")
+                            sum_percent += 0
 
+                self.resume_table.insert(parent='', index='end', iid=0, text="Parent", values=amount_for_table)
+
+                self.resume_table.insert(parent='', index='end', iid=1, text="Parent", values=percent_for_table)
+
+                self.resume_table.pack()
+
+    """ Method that adds the database data to the resume table"""
     def load_resume_data_to_table(self):
         expense_types = self.serve.get_expense_type_names()
         resume_data = self.serve.get_resume_data(('01',))
