90d258436f220029f1b15606c4c6ebd5bba2236d
David Leonardo <dlpuerta@gmail.com>
2022-03-28 23:16:47 -0500

    Trying to add unit tests. First test working

--- test/test_entities.py
+++ test/test_entities.py
@@ -6,14 +6,10 @@ from unittest import TestCase
 
 class TestEntities(TestCase):
 
-    def setup(self):
-        self.pocket = Pocket(1,
-                             'Account',
-                             10000)
+    def setUp(self):
+        self.pocket = Pocket(pocket_id=1,
+                             name='Account',
+                             amount=10000)
 
     def test_pocket(self):
         self.assertEqual(self.pocket.get_amount(), 10000)
-
-    if __name__ == "__main__":
-        suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestEntities)
-        unittest.TextTestRunner().run(suite)
