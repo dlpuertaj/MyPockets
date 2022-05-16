7041a7286bf63f7a515221b3d25a9cb02678471d
David Leonardo <dlpuerta@gmail.com>
2022-05-15 21:01:19 -0500

    Grid Layout for new income or expense type form. Not done

--- frames/popup/pop_new_type.py
+++ frames/popup/pop_new_type.py
@@ -1,4 +1,5 @@
-from tkinter import Toplevel, Button, Label, Entry, OptionMenu, StringVar
+from tkinter import Toplevel, Button, Label, Entry, OptionMenu, StringVar, E, W
+
 
 class PopNewType(Toplevel):
     """ Class that creates the popup for creating, updating or deleting a pocket"""
@@ -11,24 +12,26 @@ class PopNewType(Toplevel):
 
     def create_and_show_popup(self,serve):
 
-        type_name_label = Label(self, text="Name")
+        type_name_label = Label(self, text="Name: ")
         type_name_entry = Entry(self)
 
-        type_note_label = Label(self, text="Note")
+        type_note_label = Label(self, text="Note: ")
         type_note_entry = Entry(self)
 
         save_button = Button(self,text="Save",
                              command=lambda: self.save(serve,type_name_entry.get(),type_note_entry.get()))
         cancel_button = Button(self,text="Close", command=self.destroy)
 
-        type_name_label.pack()
-        type_name_entry.pack()
+        title_label = Label(self, text="==== New " + str(self.type) + " ====")
+        title_label.grid(column=0,row=0,columnspan=2)
+        type_name_label.grid(column=0,row=1)
+        type_name_entry.grid(column=1,row=1)
 
-        type_note_label.pack()
-        type_note_entry.pack()
+        type_note_label.grid(column=0,row=2)
+        type_note_entry.grid(column=1,row=2)
 
-        save_button.pack()
-        cancel_button.pack()
+        save_button.grid(column=0,row=3,pady=7,sticky=(E, W))
+        cancel_button.grid(column=1,row=3,pady=7,sticky=(E, W))
 
     def save(self,serve,name, note):
         if self.type:
