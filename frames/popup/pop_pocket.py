c42f2733d7cd5a9bf1083d944b4e3ae035bddfa3
David Leonardo <dlpuerta@gmail.com>
2022-07-20 21:33:08 -0500

    Working on edit pockets form

--- frames/popup/pop_pocket.py
+++ frames/popup/pop_pocket.py
@@ -1,4 +1,4 @@
-from tkinter import Toplevel, Button, Label, Entry, E, W
+from tkinter import Toplevel, Button, Label, Entry, E, W, OptionMenu, StringVar
 
 
 class PopPocket(Toplevel):
@@ -12,6 +12,16 @@ class PopPocket(Toplevel):
 
     def create_and_show_popup(self, serve):
 
+        if not self.new_or_edit:
+            pocket_options = []
+            pockets = serve.get_pockets()
+            for pocket in pockets:
+                pocket_options.append(pocket.name)
+            # Get list of pockets from db
+            clicked_source_pocket = StringVar()
+            self.add_select_dropdown(pocket_options, clicked_source_pocket, "Pocket: ", 0)
+            # Create dropdown of pockets
+
         pocket_name_label = Label(self, text="Name: ")
         pocket_name_entry = Entry(self)
 
@@ -23,14 +33,17 @@ class PopPocket(Toplevel):
 
         close_button = Button(self, text="Close", command=self.destroy)
 
-        pocket_name_label.grid(column=0,row=0,sticky=E)
-        pocket_name_entry.grid(column=1, row=0,sticky=E)
+        grid_row = 0
+        if not self.new_or_edit:
+            grid_row = 1
+        pocket_name_label.grid(column=0,row=grid_row,sticky=E)
+        pocket_name_entry.grid(column=1, row=grid_row,sticky=E)
 
-        pocket_amount_label.grid(column=0, row=1,sticky=E)
-        pocket_amount_entry.grid(column=1, row=1,sticky=E)
+        pocket_amount_label.grid(column=0, row=grid_row + 1,sticky=E)
+        pocket_amount_entry.grid(column=1, row=grid_row + 1,sticky=E)
 
-        save_button.grid(column=0, row=2,sticky=(E,W))
-        close_button.grid(column=1, row=2,sticky=(E,W))
+        save_button.grid(column=0, row=grid_row + 2,sticky=(E,W))
+        close_button.grid(column=1, row=grid_row + 2,sticky=(E,W))
 
     def save_pocket(self, serve, pocket_name, pocket_amount):
 
@@ -52,3 +65,10 @@ class PopPocket(Toplevel):
             return True
         else:
             return False
+
+    def add_select_dropdown(self, options, clicked, label,grid_row):
+        clicked.set(options[0])
+        type_label = Label(self, text=label)
+        type_label.grid(column=0,row=grid_row,sticky=(E,W))
+        dropdown = OptionMenu(self, clicked, *options)
+        dropdown.grid(column=1,row=grid_row,sticky=(E,W))
