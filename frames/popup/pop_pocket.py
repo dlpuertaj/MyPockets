386767415b25d2a81842a73558d0657951330620
David Leonardo <dlpuerta@gmail.com>
2022-07-22 00:35:01 -0500

    Working on edit pockets form

--- frames/popup/pop_pocket.py
+++ frames/popup/pop_pocket.py
@@ -6,6 +6,9 @@ class PopPocket(Toplevel):
 
     def __init__(self, root, new_or_edit):
         Toplevel.__init__(self, root)
+        self.clicked_pocket = StringVar()
+        self.pocket_amount_entry = Entry(self)
+        self.pocket_name_entry = Entry(self)
         self.new_or_edit = new_or_edit
         self.root = root
         self.grab_set()
@@ -17,19 +20,17 @@ class PopPocket(Toplevel):
             pockets = serve.get_pockets()
             for pocket in pockets:
                 pocket_options.append(pocket.name)
-            # Get list of pockets from db
-            clicked_source_pocket = StringVar()
-            self.add_select_dropdown(pocket_options, clicked_source_pocket, "Pocket: ", 0)
-            # Create dropdown of pockets
 
+            self.add_select_dropdown(pocket_options, "Pocket: ", 0)
+            self.pocket_amount_entry.config()
         pocket_name_label = Label(self, text="Name: ")
-        pocket_name_entry = Entry(self)
+        self.pocket_name_entry = Entry(self)
 
         pocket_amount_label = Label(self, text="Initial Amount: ")
-        pocket_amount_entry = Entry(self)
+        self.pocket_amount_entry = Entry(self)
 
         save_button = Button(self, text="Save", command=lambda: self.save_pocket(
-            serve, pocket_name_entry.get(), pocket_amount_entry.get()))
+            serve, self.pocket_name_entry.get(), self.pocket_amount_entry.get()))
 
         close_button = Button(self, text="Close", command=self.destroy)
 
@@ -37,10 +38,10 @@ class PopPocket(Toplevel):
         if not self.new_or_edit:
             grid_row = 1
         pocket_name_label.grid(column=0,row=grid_row,sticky=E)
-        pocket_name_entry.grid(column=1, row=grid_row,sticky=E)
+        self.pocket_name_entry.grid(column=1, row=grid_row,sticky=E)
 
         pocket_amount_label.grid(column=0, row=grid_row + 1,sticky=E)
-        pocket_amount_entry.grid(column=1, row=grid_row + 1,sticky=E)
+        self.pocket_amount_entry.grid(column=1, row=grid_row + 1,sticky=E)
 
         save_button.grid(column=0, row=grid_row + 2,sticky=(E,W))
         close_button.grid(column=1, row=grid_row + 2,sticky=(E,W))
@@ -66,9 +67,15 @@ class PopPocket(Toplevel):
         else:
             return False
 
-    def add_select_dropdown(self, options, clicked, label,grid_row):
-        clicked.set(options[0])
+    def add_select_dropdown(self, options, label,grid_row):
+        self.clicked_pocket.set(options[0])
+        self.clicked_pocket.trace("w", self.pocket_selection_callback)
         type_label = Label(self, text=label)
         type_label.grid(column=0,row=grid_row,sticky=(E,W))
-        dropdown = OptionMenu(self, clicked, *options)
+        dropdown = OptionMenu(self, self.clicked_pocket, *options)
         dropdown.grid(column=1,row=grid_row,sticky=(E,W))
+
+    def pocket_selection_callback(self,*clicked_item):
+
+        self.pocket_name_entry.insert(0,self.clicked_pocket.get())
+        self.pocket_amount_entry.insert(0,"")
