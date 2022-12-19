5524d3bc8e4614e27b372d3979df1c00d9efa63d
David Leonardo <dlpuerta@gmail.com>
2022-12-19 08:26:16 -0500

    finished pocket edit and some bug fix

--- frames/popup/pop_pocket.py
+++ frames/popup/pop_pocket.py
@@ -40,14 +40,15 @@ class PopPocket(Toplevel):
         close_button.grid(column=1, row=grid_row + 2, sticky=(E, W))
 
     def add_edit_pocket_components(self, serve):
-        pocket_options = []
-        self.pockets = serve.get_pockets()
-        for pocket in self.pockets:
-            pocket_options.append(pocket.name)
+        pocket_options = self.get_pockets(serve)
         self.add_select_dropdown(pocket_options, "Pocket: ", 0)
         self.pocket_name_entry.insert(0, str(self.pockets[0].get_name()))
         self.pocket_amount_entry.insert(0, str(self.pockets[0].get_amount()))
         self.pocket_amount_entry.config(state="disabled")
+
+    def update_edit_pocket_components(self, serve):
+        pockets_options = self.get_pockets(serve)
+        self.add_select_dropdown(pockets_options, "Pocket: ", 0)
 
     def save_pocket(self, serve, pocket_name, pocket_amount):
 
@@ -61,6 +62,7 @@ class PopPocket(Toplevel):
                         if self.clicked_pocket_id is None:
                             self.clicked_pocket_id = self.get_pocket_from_name(self.clicked_pocket.get()).get_id()
                         serve.update_pocket(pocket_name, self.clicked_pocket_id)
+                        self.update_edit_pocket_components(serve)
                     serve.show_popup_message(self.root, "Success!")
 
                 else:
@@ -94,6 +96,14 @@ class PopPocket(Toplevel):
         pocket = self.get_pocket_from_callback(self.clicked_pocket.get())
         self.pocket_amount_entry.insert(0, pocket.get_amount())
 
+    def get_pockets(self,serve):
+        pocket_options = []
+        self.pockets = serve.get_pockets()
+        for pocket in self.pockets:
+            pocket_options.append(pocket.name)
+
+        return pocket_options
+
     def get_pocket_from_callback(self, pocket_name):
         for pocket in self.pockets:
             if pocket_name == pocket.get_name():
@@ -104,3 +114,4 @@ class PopPocket(Toplevel):
         for pocket in self.pockets:
             if pocket_name == pocket.get_name():
                 return pocket
+
