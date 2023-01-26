a43f00cac44c9487e10e5a28d7bf43d57918e686
David Leonardo <dlpuerta@gmail.com>
2023-01-25 23:22:51 -0500

    pocket transaction

---
+++ entities/pocket_transaction.py
@@ -0,0 +1,9 @@
+class PocketTransaction:
+    def __init__(self, pocket_id, source_pocket_id, income_type_id, expense_type_id, amount, date):
+        self.pocket_id = pocket_id
+        self.source_pocket_id = source_pocket_id
+        self.income_type_id = income_type_id
+        self.expense_type_id = expense_type_id
+        self.amount = amount
+        self.date = date
+
